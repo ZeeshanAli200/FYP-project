519a851a4a87142bda60627639723644c85ed781
ZeeshanAli200 <zeeshan998811@gmail.com>
2022-02-19 03:07:20 +0500

    latest changes made

--- js/admin.js
+++ js/admin.js
@@ -32,7 +32,6 @@ async function getAllMoviesCount() {
 }
 getAllMoviesCount();
 
-
 // user page
 
 async function onLoadUser() {
@@ -61,39 +60,79 @@ async function onLoadActiveUser() {
     const userData = users.docs.map((doc) => doc.data());
     const elem = getElement("active-user-table");
     var tr = "";
-    for (let index = 0; index < userData.length; index++) {
-      if (userData[index].isActive) {
+    userData?.map((userdt, index) => {
+      if (userdt?.isActive) {
         tr += `<tr>
-      <th scope="row">${index}</th>
-      <td>${userData[index].userName || "-"}</td>
-      <td>${userData[index].email || "-"}</td>
-      <td>${userData[index].gender || "-"}</td>
-      <td>${userData[index].age || "-"}</td>
-      <td>${userData[index].role || "-"}</td>
-      <td>${userData[index].isActive || "-"}</td>
-    </tr>`;
+<th scope="row">${index}</th>
+<td>${userdt.userName || "-"}</td>
+<td>${userdt.email || "-"}</td>
+<td>${userdt.gender || "-"}</td>
+<td>${userdt.age || "-"}</td>
+<td>${userdt.role || "-"}</td>
+<td>${userdt.isActive || "-"}</td>
+<td style="cursor: pointer;"><a class="navbar-brand " onclick="showAnalyticspage(${
+  String(userdt.UID)
+        })">Show Analytics</a></td>
+</tr>`;
       }
-    }
+    });
+
     elem.innerHTML = tr;
   } catch (error) {}
+}
+function showAnalyticspage(id) {
+  console.log(id);
+  // window.location.replace(`../adminpages/analytics.html`)
 }
 async function onLoadMovies() {
   try {
     const movies = await db.collection("movieList").get();
     const movieData = movies.docs.map((doc) => doc.data());
     const elem = getElement("movie-table");
-  
+
     var tr = "";
     for (let index = 0; index < movieData.length; index++) {
-        tr += `<tr>
+      tr += `<tr>
       <th scope="row">${index}</th>
       <td>${movieData[index].VideoName || "-"}</td>
-      <td><img class="thumb-img" src="${movieData[index].VideoImageThumbnail||movieData[index].VideoOnDemandThumb}"/></td>
+      <td><img class="thumb-img" src="${
+        movieData[index].VideoImageThumbnail ||
+        movieData[index].VideoOnDemandThumb
+      }"/></td>
     </tr>`;
     }
-    console.log("trv: ",tr);
+    console.log("trv: ", tr);
     elem.innerHTML = tr;
   } catch (error) {
     console.log("phat gyaaa");
   }
+}
+
+// ***************************************************************analytics
+async function renderAnalytic() {
+  try {
+    // const users = await db.collection("streamUsers").get();
+    // const userData = users.docs.map((doc) => doc.data());
+    // db.collection("streamUsers").doc("SF")
+    // .onSnapshot((doc) => {
+    //     var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+    //     console.log(source, " data: ", doc.data());
+    // });
+    const elem = getElement("analytic-table");
+    var tr = "";
+    // for (let index = 0; index < userData.length; index++) {
+    //   if (userData[index].isActive) {
+    //     tr += `<tr>
+    //   <th scope="row">${index}</th>
+    //   <td>${userData[index].userName || "-"}</td>
+    //   <td>${userData[index].email || "-"}</td>
+    //   <td>${userData[index].gender || "-"}</td>
+    //   <td>${userData[index].age || "-"}</td>
+    //   <td>${userData[index].role || "-"}</td>
+    //   <td>${userData[index].isActive || "-"}</td>
+    // </tr>`;
+    //   }
+    // }
+    // elem.innerHTML = tr;
+  } catch (error) {}
 }
